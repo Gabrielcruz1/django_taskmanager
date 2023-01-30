fb638d57ce4ef742bcfcc1240134b3e20c9cac05
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-30 12:32:38 -0800

    path added for task list. template created. working in broswer needs conditional rendering

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('about/', views.About.as_view(), name="about")
+    path('about/', views.About.as_view(), name="about"),
+    #ROUTE IS TASKS PLURAL 
+    path('tasks/', views.TaskList.as_view(), name="task_list" )
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -14,3 +14,19 @@ class Home(TemplateView):
 #ABOUT PAGE CLASS W/ FUNCTION 
 class About(TemplateView):
     template_name = "about.html"
+
+
+class Task:
+    def __init__(self, title):
+        self.title = title 
+
+
+class TaskList(TemplateView):
+    template_name = "task_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tasks"] = Task
+        return context
+
+
