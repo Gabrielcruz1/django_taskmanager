4df5bc5dcc3ff4de699500ac2a6f79332fea46cc
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-30 12:46:13 -0800

    rendered tasks on page

--- main_app/views.py
+++ main_app/views.py
@@ -2,23 +2,18 @@ from django.shortcuts import render
 from django.views import View #HANDLES REQUESTS
 from django.http import HttpResponse #HANDLES RESPONSE
 from django.views.generic.base import TemplateView
-
+from .models import Task 
 
 # Create your views here.
 
 class Home(TemplateView):
     template_name = "home.html"
 
-
-
 #ABOUT PAGE CLASS W/ FUNCTION 
 class About(TemplateView):
     template_name = "about.html"
 
 
-class Task:
-    def __init__(self, title):
-        self.title = title 
 
 
 class TaskList(TemplateView):
@@ -26,7 +21,7 @@ class TaskList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["tasks"] = Task
+        context["tasks"] = Task.objects.all()
         return context
 
 
