c13aa695f35696013d42ac4f424c3faf62386545
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-30 10:00:29 -0800

    Added Task model. Made migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
