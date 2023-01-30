e67876d9f0d0f103eeed51576051b3ab43f58243
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-30 11:50:47 -0800

    added sql file inserted data into psql

---
+++ db/create_task.sql
@@ -0,0 +1,14 @@
+INSERT INTO main_app_task (title) VALUES ('Finish Project');
+INSERT INTO main_app_task (title) VALUES ('Go to Grocery store');
+INSERT INTO main_app_task (title) VALUES ('Learn New coding Language');
+INSERT INTO main_app_task (title) VALUES ('Finish Portfolio');
+INSERT INTO main_app_task (title) VALUES ('Finish Resume');
+
+
+
+
+
+
+
+
+ 
