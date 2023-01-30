c06f4e3159ce5257685230c24a6f4f9493784e13
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-30 07:57:43 -0800

    added template view for home

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views import View #HANDLES REQUESTS
 from django.http import HttpResponse #HANDLES RESPONSE
+from django.views.generic.base import TemplateView
 
 
 # Create your views here.
 
-class Home(View):
-    def get (self, request):
-        return HttpResponse('Task Manager Home')
+class Home(TemplateView):
+    template_name = "home.html"
+
 
 
 #ABOUT PAGE CLASS W/ FUNCTION 
