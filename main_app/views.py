97fa96cbca16e9a51119163782f8effd40b2b765
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-30 20:25:38 -0800

    search bar updated

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View #HANDLES REQUESTS
 from django.http import HttpResponse #HANDLES RESPONSE
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView 
 from .models import Task 
 
 # Create your views here.
@@ -21,7 +21,11 @@ class TaskList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["tasks"] = Task.objects.all()
+        title = self.request.GET.get("title")
+        if title != None:
+            context["tasks"] = Task.objects.filter(title__icontains=title)
+        else:
+            context["tasks"] = Task.objects.all()
         return context
 
 
