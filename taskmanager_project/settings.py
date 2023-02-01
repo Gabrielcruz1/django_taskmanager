d8fcca93138573887666496699654762decacdc4
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-02-01 12:22:16 -0800

    Created gitignore and dotenv & hid secret_key

--- taskmanager_project/settings.py
+++ taskmanager_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os 
+import dotenv
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a*z%p(2k*7t=!o4a-4b!p4wmlnpvocbuwyj+6^88grte^%#hog'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
