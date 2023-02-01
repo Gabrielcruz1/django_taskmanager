8eb497b325ba4fe58354bd688f1c1a6b7a3eea1c
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-02-01 13:19:52 -0800

    Pushing for deploy

--- taskmanager_project/settings.py
+++ taskmanager_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os 
-import dotenv
 from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
