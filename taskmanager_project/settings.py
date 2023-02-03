002ea1d6aec15fbb7121da34641e7f82e367941d
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-02-02 21:04:48 -0800

    added form styling with form crispy

--- taskmanager_project/settings.py
+++ taskmanager_project/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app',
+    'crispy_forms'
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
